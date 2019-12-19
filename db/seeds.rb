15b0b1d1da28a8a566baaf2425e37c62ae05238f
Jason McKenna <jasonmckenna00@gmail.com>
2019-12-19 15:11:50 -0500

    added open-uri to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require "open-uri"
 Video.destroy_all
 User.destroy_all
 
