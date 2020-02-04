8771f00f55ad9ba8ae262335ddc3dd06ee2b551b
Jason McKenna <jasonmckenna00@gmail.com>
2020-02-03 21:20:26 -0500

    index page refactoring

--- frontend/components/videos/video_index.jsx
+++ frontend/components/videos/video_index.jsx
@@ -14,43 +14,87 @@ class VideoIndex extends React.Component{
         this.props.fetchAllVideos()
     }
 
-    // componentDidUpdate(prevProps) {
-    //     if (prevProps.location.pathname !== this.props.history.location.pathname) {
-    //         Promise.all([this.props.fetchAllUsers(), this.props.fetchAllVideos()]);
-    //     }
-    //   }  
+    random(amount,videoCount){
 
+        let recommended = new Set();
+        for (let i = 0; i < amount; i++){
+            const rng = Math.floor(Math.random()*videoCount);
+            if (recommended.has(rng)){
+                i -= 1;
+                continue
+            }
+            recommended.add(rng)
+        }
+        return recommended
+        
+    }
 
     render(){
+            if (!this.props.videos.length) return null;
             const { videos, users, searchVids, loading, searched } = this.props
-            const videosLis = videos.map( video => <VideoIndexItem 
-                                                    key={video.id} 
-                                                    video={video} 
-                                                    user={users[video.user_id]} 
-                                                    loading={loading}/>)  
-            const searchLis = searchVids.map ( (video,i) => {
-                return <VideoIndexItem 
-                            key={i} 
-                            video={video} 
-                            user={users[video.user_id]} 
-                            loading={loading}/>  
-                })
-            
-            
-            // debugger  
-            const videoOutput = searched ? searchLis : videosLis;
+            const allUsersArray = Object.values(users)
+            let recommended = this.random(8, videos.length);
+            let recommendedLis = [];
+            const user0 = allUsersArray[0];
+            const user1 = allUsersArray[1];
+            const user2 = allUsersArray[2];
+            let user0Videos = [];
+            let user1Videos = [];
+            let user2Videos = [];
+
+
+
+            videos.forEach( video => {
+                const videoLi = <VideoIndexItem 
+                                key={video.id} 
+                                video={video} 
+                                user={users[video.user_id]} 
+                                />
+
+                if (recommended.has(video.id)){
+                    recommendedLis.push(videoLi)
+                }
+    
+                if (video.user_id === user0.id && user0Videos.length < 9){
+                    user0Videos.push(videoLi)
+                }
+                if (video.user_id === user1.id  && user1Videos.length < 9){
+                    user1Videos.push(videoLi)
+                } 
+                if (video.user_id === user2.id  && user2Videos.length < 9){
+                    user2Videos.push(videoLi)
+                }
+          
+            })
+
+            // console.log(randomUserVideos2)
             return <>
                 <div className='splashpage'>
-                    {/* <SideBarContainer /> */}
-                    <div>
                         <h2 className='video-index-header'> Recommended</h2>
-                        <ul className='videos-container'> {videoOutput} </ul>
-                    </div>
+                        <div id='recommendedLis'className='videos-container'> {recommendedLis} </div>
+
+                        <div id='user0'className='videos-container random-user-videos'> 
+        
+                            <h2 className='video-index-section-header'>{user0.first_name + ' ' + user0.last_name}</h2>
+                            <div className='videos-container'> {user0Videos} </div>
+                            
+                        </div>
+                        <div id='user1'className='random-user-videos'>
+                            <h2 className='video-index-section-header'>{user1.first_name + ' ' + user1.last_name}</h2>
+                            <div className='videos-container' >{user1Videos} </div>
+                            
+                        </div>
+                        <div id='user2'className='videos-container random-user-videos'> 
+                            <h2 className='video-index-section-header'>{user2.first_name + ' ' + user2.last_name}</h2>
+                            <div className='videos-container'> {user2Videos} </div>
+                            
+                        </div>
+
+
+                    
                 </div>
             </>
              
-                
-            // }
     }
 }
 
--- frontend/reducers/side_bar_ui_reducer.js
+++ frontend/reducers/side_bar_ui_reducer.js
@@ -1,7 +1,7 @@
 import {OPEN_SIDE_BAR, CLOSE_SIDE_BAR} from '../actions/ui_actions'
 
 
-export default (state = {isOpen: false}, action)=>{
+export default (state = {isOpen: true}, action)=>{
     switch(action.type){
         case OPEN_SIDE_BAR:
             return {isOpen: true}
